7ba104484b2a3811ec01230f19e75b845a70b932
chiggum <codechiggum@gmail.com>
2014-10-17 23:16:15 +0530

    INVCNT done

--- INVCNT/INVCNT.cpp
+++ INVCNT/INVCNT.cpp
@@ -100,66 +100,59 @@ typedef unsigned long long u64;
 inline int toInt(string s) {int i;stringstream (s)>>i;return i;}
 inline string toString(int i) {string s;stringstream ss;ss<<i;ss>>s;return s;}
 
-void update_tree(s64 *tree, s64* lazy, s64 node, s32 a, s32 b, s32 i, s32 j, s32 value) {
-	if(a > b || a > j || b < i)
-		return;
-    
-    if(a >= i && b <= j) {
-    	lazy[node]+=value;
-    	return;
+#define INFINITY 10000001
+
+s64 modifMerge(s32 *arr, s32 a, s32 mid, s32 b) {
+    s32 n1 = mid - a + 1;
+    s32 n2 = b - mid;
+    s32 *l = new s32[n1+1];
+    s32 *m = new s32[n2+1];
+    s32 i;
+    s64 invcnt = 0;
+    REP(i, n1) {
+        l[i] = arr[a+i];
     }
-    s32 mid = (a+b)/2;
-	update_tree(tree, lazy, 1+node*2, a, mid, i, j, value);
-	update_tree(tree, lazy, 2+node*2, 1+mid, b, i, j, value);
- 
-	tree[node] = tree[node*2+1]+tree[node*2+2] + (mid-a+1)*lazy[2*node+1] + (b-mid)*lazy[2*node+2];
+    REP(i, n2) {
+        m[i] = arr[mid + 1 + i];
+    }
+    l[n1] = INFINITY;
+    m[n2] = INFINITY;
+    s32 j=0,k=0;
+    rep(i, a, b+1) {
+        if(l[j] < m[k]) {
+            arr[i]=l[j++];
+        } else {
+            arr[i]=m[k++];
+            invcnt += n1 - j;
+        }
+    }
+    return invcnt;
 }
 
-s64 query_tree(s64 *tree, s64 *lazy, s64 node, s32 a, s32 b, s32 i, s32 j) {	
-	if(a > b || a > j || b < i)
-		return 0;
-
-	s64 ret = (min(b,j)-max(a,i)+1)*lazy[node];
-
-	if(a >= i && b <= j)
-		return ret+tree[node];
- 
-	s64 q1 = query_tree(tree, lazy, 1+node*2, a, (a+b)/2, i, j);
-	s64 q2 = query_tree(tree, lazy, 2+node*2, 1+(a+b)/2, b, i, j);
-
-	s64 res = ret + q1 + q2;
-	return res;
+s64 modifMergeSort(s32 *arr, s32 a, s32 b) {
+    s64 invcnt = 0;
+    if(a < b) {
+        s32 mid = (a+b)/2;
+        s64 invcnt1 = modifMergeSort(arr, a, mid);
+        s64 invcnt2 = modifMergeSort(arr, mid + 1, b);
+        invcnt = modifMerge(arr, a, mid, b);
+        invcnt += invcnt1 + invcnt2;
+    }
+    return invcnt;
 }
 
 int main() {
-	s64 T;
-	s64 t;
-	scanll(T);
-	REP(t, T) {
-		s32 N, C, n;
-		scanl(N);
-		scanl(C);
-		s64 numElems = 2 * pow(2, ceil(log10(N)/log10(2))) - 1;
-		s64 *myArray = new s64[numElems];
-		s64 *lazy = new s64[numElems];
-		memset(lazy, 0, numElems * sizeof(s64));
-		memset(myArray, 0, numElems * sizeof(s64));
-		REP(n, C) {
-			s32 type, a, b, v;
-			scanl(type);
-			scanl(a);
-			scanl(b);
-			a--;
-			b--;
-			if(type == 0) {
-				scanl(v);
-				update_tree(myArray, lazy, 0, 0, N-1, a, b, v);
-			} else {
-				u64 sum;
-				sum=query_tree(myArray, lazy, 0, 0, N-1, a, b);
-				printf("%lld\n", sum);
-			}
-		}
-	}
-	return 0;
+    s32 T;
+    scanl(T);
+    while(T--) {
+        s32 N;
+        scanl(N);
+        s32 *arr = new s32[N];
+        s32 i;
+        REP(i, N) {
+            scanl(arr[i]);
+        }
+        s64 invcnt = modifMergeSort(arr, 0, N-1);
+        printf("%lld\n", invcnt);
+    }
 }
